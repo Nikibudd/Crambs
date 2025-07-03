cb7c800bbf5d694945a77baa55e819a607fb5199
njbro <nj.broennimann@gmail.com>
2025-07-03 15:43:22 +0200

    implemented scene navigation

---
+++ src/main/kotlin/io/github/nikibudd/crambs/navigation/Navigator.kt
@@ -0,0 +1,25 @@
+package io.github.nikibudd.crambs.navigation
+
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Navigator {
+
+    private val stage: Stage
+    private var scenes: MutableMap<String, Scene>
+
+    constructor(stage: Stage) {
+        this.stage = stage
+        this.scenes = HashMap()
+    }
+
+    fun registerScene(name: String, scene: Scene) {
+        scenes[name] = scene
+    }
+
+    fun navigateTo(name: String) {
+        stage.scene = scenes[name]
+        stage.show()
+    }
+
+}
